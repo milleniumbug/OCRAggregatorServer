85a75036622dc95c37fac8d74016ee8bbc4eb9e1
milleniumbug <milleniumbug@protonmail.com>
2022-02-09 20:29:08 +0100

    Add infra for sorting modes

--- main.py
+++ main.py
@@ -10,7 +10,14 @@ import cv2
 import np
 
 
-def create_darknet_detector():
+def create_y_coordinate_sorter():
+    def sorter(image_file, detections):
+        return sorted(detections, key=lambda d: d[1])
+
+    return sorter
+
+
+def create_darknet_detector(detection_sorter):
     import darknet
     Detector = darknet.load_darknet_detector()
     detector = Detector(
@@ -20,14 +27,14 @@ def create_darknet_detector():
 
     def detect(image_file):
         result = detector.detect(image_file)
-        return [(x1 - 10, y1 - 10, x2 + 10, y2 + 10) for x1, y1, x2, y2 in sorted(result, key=lambda d: d[1])]
+        return [(x1 - 10, y1 - 10, x2 + 10, y2 + 10) for x1, y1, x2, y2 in result]
 
     return detect
 
 
 def create_manga_ocr():
     from manga_ocr import MangaOcr
-    mocr = MangaOcr(force_cpu=True)
+    mocr = MangaOcr()
 
     def ocr(image_file):
         with PIL.Image.open(image_file) as image:
@@ -55,22 +62,29 @@ def create_tesseract_ocr():
 
 def create_combined_detector_ocr(ocr, detector):
     def combined(image_file):
-        with io.BytesIO(image_file.read()) as buffered:
-            detections = detector(buffered)
-            buffered.seek(0, io.SEEK_SET)
-            with PIL.Image.open(buffered) as image:
-                for detection in detections:
-                    region = image.crop(detection)
-                    with io.BytesIO() as region_file:
-                        region.save(region_file, 'PNG')
-                        region_file.seek(0, io.SEEK_SET)
-                        text = ocr(region_file)
-                        yield {"text": text, "rect": detection}
+        detections = detector(image_file)
+        image_file.seek(0, io.SEEK_SET)
+        with PIL.Image.open(image_file) as image:
+            for detection in detections:
+                region = image.crop(detection)
+                with io.BytesIO() as region_file:
+                    region.save(region_file, 'PNG')
+                    region_file.seek(0, io.SEEK_SET)
+                    text = ocr(region_file)
+                    yield {"text": text, "rect": detection}
 
     return lambda image_file: list(combined(image_file))
 
 
-def create_engines(ocr_mode: str, detector_mode: str, combined_mode: Union[str, None]):
+def create_engines(
+        ocr_mode: str,
+        detector_mode: str,
+        combined_mode: Union[str, None],
+        detection_sorter_mode: str):
+
+    if detection_sorter_mode == 'y_coordinate':
+        sorter = create_y_coordinate_sorter()
+
     if ocr_mode == 'manga-ocr':
         ocr = create_manga_ocr()
     elif ocr_mode == 'tesseract':
@@ -79,7 +93,7 @@ def create_engines(ocr_mode: str, detector_mode: str, combined_mode: Union[str, 
         ocr = None
 
     if detector_mode == 'darknet':
-        detector = create_darknet_detector()
+        detector = create_darknet_detector(sorter)
     else:
         detector = None
 
@@ -103,17 +117,20 @@ def create_app(ocr, detector, combined_detector_ocr, config=None):
     @app.route("/ocr", methods=['POST'])
     def issue_ocr():
         image_file = request.files['input_image']
-        return jsonify(str(ocr(image_file.stream)))
+        with io.BytesIO(image_file.stream.read()) as buffered:
+            return jsonify(str(ocr(buffered)))
 
     @app.route("/detect", methods=['POST'])
     def issue_textbox_detection():
         image_file = request.files['input_image']
-        return jsonify(detector(image_file.stream))
+        with io.BytesIO(image_file.stream.read()) as buffered:
+            return jsonify(detector(buffered))
 
     @app.route("/detect_ocr", methods=['POST'])
     def issue_textbox_detection_with_ocr():
         image_file = request.files['input_image']
-        return jsonify(combined_detector_ocr(image_file.stream))
+        with io.BytesIO(image_file.stream.read()) as buffered:
+            return jsonify(combined_detector_ocr(buffered))
 
     return app
 
@@ -125,12 +142,14 @@ def main():
     parser.add_argument("--detection-mode", action="store", default="darknet")
     parser.add_argument("--ocr-mode", action="store", default="manga-ocr")
     parser.add_argument("--combined-detection-ocr-mode", action="store", default=None)
+    parser.add_argument("--detection-ordering-mode", action="store", default='y_coordinate')
 
     args = parser.parse_args()
     ocr, detector, combined_detector_ocr = create_engines(
         args.ocr_mode,
         args.detection_mode,
-        args.combined_detection_ocr_mode)
+        args.combined_detection_ocr_mode,
+        args.detection_ordering_mode)
     created_app = create_app(ocr, detector, combined_detector_ocr)
     created_app.run(host=args.host, port=args.port, use_reloader=False)
 
